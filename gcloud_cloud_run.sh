8a944a8d797d2f1ac89d9ec95f26619668061385
Weiyuan <weiyuan.lane@gmail.com>
2019-08-14 00:25:06 +0800

    [Ops] Update cloud run deploy script

--- gcloud_cloud_run.sh
+++ gcloud_cloud_run.sh
@@ -2,5 +2,5 @@
 
 PROJECT_ID=`gcloud config list --format 'value(core.project)'`
 
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/slides -q
-gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld -q --allow-unauthenticated
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/helloworld:v2 -q
+gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld:v2 -q --allow-unauthenticated
