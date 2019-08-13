2ed1ea4e18a3a88f16dd226e173f19869bfdeffc
Weiyuan <weiyuan.lane@gmail.com>
2019-08-14 00:53:11 +0800

    [Ops] Added versioning for deploying on this branch

--- gcloud_cloud_run.sh
+++ gcloud_cloud_run.sh
@@ -2,5 +2,5 @@
 
 PROJECT_ID=`gcloud config list --format 'value(core.project)'`
 
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/helloworld:v1 -q
-gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld:v1 -q --allow-unauthenticated --platform="managed" --region="us-central1" helloworld
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/helloworld:v2 -q
+gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld:v2 -q --allow-unauthenticated --platform="managed" --region="us-central1" helloworld
