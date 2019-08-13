f7f7973cdb2f114357d79d2c1c2e300dcba011f6
Weiyuan <weiyuan.lane@gmail.com>
2019-08-14 00:19:35 +0800

    [Ops] Update app engine deploy script

--- gcloud_app_engine.sh
+++ gcloud_app_engine.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-PROJECT_ID=`gcloud config list --format 'value(core.project)'`
-
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/slides -q
-gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld -q
+gcloud app deploy -q
