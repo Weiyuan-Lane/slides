72463b98179a653dc6959d84f111d8c3123dd562
Weiyuan <weiyuan.lane@gmail.com>
2019-08-14 00:22:33 +0800

    [Ops] Add "--allow-unauthenticated" flag to cloud run

--- gcloud_cloud_run.sh
+++ gcloud_cloud_run.sh
@@ -3,4 +3,4 @@
 PROJECT_ID=`gcloud config list --format 'value(core.project)'`
 
 gcloud builds submit --tag gcr.io/${PROJECT_ID}/slides -q
-gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld -q
+gcloud beta run deploy --image gcr.io/${PROJECT_ID}/helloworld -q --allow-unauthenticated
