997d925f338129ee92ca408eb1a81f0d303fe4d8
Weiyuan Liu <weiyuan.lane@gmail.com>
2019-10-22 02:31:04 +0800

    DEPLOY

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const helmet = require('helmet')
+const next = require('next')
+const path = require('path')
+
+// Controller imports
+const catController = require(path.resolve('server/controllers/cats-controller.js'))
+
+const port = 8080
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+    server.use(helmet())
+
+    server.set('view engine', 'ejs')
+    server.use("/scripts", express.static(path.join(__dirname, '..', 'client-assets')));
+
+    server.get('/', (_, res) => {
+      res.redirect('/events/keeping-your-hands-off-the-servers-2019-07-17')
+    })
+
+    server.get('/cats', catController.index)
+    server.get('/cats/:id', catController.show)
+
+    server.get('/heartbeat', (_, res) => {
+      res.sendStatus(200)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, err => {
+      if (err) throw err
+      console.log('[Info] Custom server ready on port ' + port)
+    })
+  })
+  .catch(e => {
+    console.log(e.stack)
+    process.exit(1)
+  })
